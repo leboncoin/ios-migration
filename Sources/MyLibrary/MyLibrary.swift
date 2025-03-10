c3713321819a70eac97987c0691bc08096f2b54b
Robin Lemaire <robin.lemaire@adevinta.com>
2025-03-10 14:38:51 +0100

    Initial init

---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct Hello {
+
+    static func helloWorld() -> String {
+        return "Hello world !"
+    }
+}
